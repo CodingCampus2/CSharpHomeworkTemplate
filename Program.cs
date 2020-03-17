5edc33b0c2ab9e7853e1e42f1d2622a2ee972c16
Tymur Antonov <tymur.antonov@gmail.com>
2020-03-17 17:38:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodingCampusCSharpHomework;
+
+namespace HomeworkTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 task1 = new Task1();
+
+            task1.Output.CheckAnswer("answer");
+        }
+    }
+}
